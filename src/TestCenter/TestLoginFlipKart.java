dc33515a777497b39752d46a0e4a2479035f68f3
Kannan Maikeffi <maikeffi@gmail.com>
2012-05-21 20:31:22 +0530

    second commit

--- src/TestCenter/TestLoginFlipKart.java
+++ src/TestCenter/TestLoginFlipKart.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class TestLoginFlipKart extends TestBaseFlipKart {
 	@Test
 	public void clikLogin() throws Exception{
-		flipKartManager.getFlipKartGate().login("maikeffi@gmail.com", "Axshan@2"); //give in your user name and password
+		flipKartManager.getFlipKartGate().login("******", "*****"); //give in your user name and password
 		
 		
 		}
